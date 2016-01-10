8313e344defe9fabae3c5eafc6b1cffea513b3eb
Levy.Xia.Home <hero0567@163.com>
2016-01-11 00:14:03 +0800

    add favorite

--- src/main/webapp/js/controllers/IndexController.js
+++ src/main/webapp/js/controllers/IndexController.js
@@ -9,6 +9,7 @@ app.controller("IndexController", function($scope, $http, $location, userService
 	
 	$scope.menu = {};
     $scope.handbook = [];
+    $scope.favor = {};
     
     $scope.size = 2;    
     $scope.type = "全部";
@@ -26,12 +27,37 @@ app.controller("IndexController", function($scope, $http, $location, userService
   	}  
     
     $scope.fetchSubTypes = function() {
-    	angular.forEach($scope.menu, function (m) {
-    		$http.get('/hb/大家电/'+m.subType+'?size=' + $scope.size).success(function(airCleaners){
-                $scope.handbook.push({name:m.subType, count:m.count, subType: airCleaners});                
+    	angular.forEach($scope.menu, function (m, index) {
+    		$http.get('/hb/大家电/'+m.subType+'?size=' + $scope.size).success(function(types){
+                $scope.handbook.push({name:m.subType, count:m.count, subType: types});  
+                if ($scope.handbook.length == $scope.menu.length){
+                	$scope.fetchFavorite();
+                }
     		});
     	});    	
   	} 
+    
+    $scope.fetchFavorite = function() {
+    	console.log("fetchFavorite" + $scope.handbook.length);
+    	if ($scope.user.id){
+    		$http.get('/favor/' + $scope.user.id).success(function(favor){
+                $scope.favor = favor;
+                angular.forEach($scope.handbook, function (hb) {
+                	angular.forEach(hb.subType, function (type) {
+                		angular.forEach(favor, function (f) {
+                			if (type.id == f.handBook.id){
+                				type.favor = "true";
+                            	console.log("type:"+type.id);
+                            	console.log("favor:"+f.handBook.id);
+                			}
+//                        	console.log("type:"+type.id);
+//                        	console.log("favor:"+f.handBook.id);
+                        });
+                    });
+                });
+    		});
+    	}
+ 	} 
     
     $scope.changeMenu = function(type, count) {
     	window.location = "/fridge?subtype="+type +"&count="+count;
