4466b698031deb2a743928b3a574ddc44120ddf4
Levy.Xia.Home <hero0567@163.com>
2016-01-26 20:43:30 +0800

    fix toplist error

--- src/main/webapp/js/controllers/FavoritesController.js
+++ src/main/webapp/js/controllers/FavoritesController.js
@@ -91,17 +91,34 @@ app.controller("FavoritesController", function($scope, $http, $location, $window
     	if (angular.isDefined(type)){
     		$http.get('/favor/'+ id + '/' + type, {params: {"subType":type, pn:$scope.page.pn, size:$scope.page.size}}).success(function(results) {  
            	 	$scope.results = results;
+				angular.forEach(results, function (result) {
+					result.favor = true;
+				});
             });  
     	}else{
     		$http.get('/favor/'+ id, {params: {pn:$scope.page.pn, size:$scope.page.size}}).success(function(results) {  
            	 	$scope.results = results;
+           	 	angular.forEach(results, function (result) {
+					result.favor = true;
+				});
             });  
     	}
  	} 
     
     $scope.addFavorite = function(uid, sub){
-    	sub.favor = true;
     	userService.addFavorite(uid, sub);
+//		if (uid){
+//			var ret = $window.confirm('确认取消收藏?');  
+//			if (!ret){
+//				return;
+//			}			
+//			$http.delete('/favor/'+uid+'/' + sub.id).success(function() {
+//				$scope.fetchFavorList($scope.user.id);        
+//			    $scope.searchSubType($scope.user.id);
+//	        });			
+//    	}else{
+//    		window.location = "/signin";
+//    	}
 	}   
     
     $scope.changeSubType = function(type, count) {
