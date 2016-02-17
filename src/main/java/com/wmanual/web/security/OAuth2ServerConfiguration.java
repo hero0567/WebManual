9199779120d1a87e3da25f3e857c949c7f0ca308
hero0567@163.com <hero0567@163.com>
2016-02-17 16:48:24 +0800

    revert oauth2

--- src/main/java/com/wmanual/web/security/OAuth2ServerConfiguration.java
+++ src/main/java/com/wmanual/web/security/OAuth2ServerConfiguration.java
@@ -1,118 +1,112 @@
-package com.wmanual.web.security;
-
-/*
- * Copyright 2014 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
-import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-@Configuration
-public class OAuth2ServerConfiguration {
-
-	private static final String RESOURCE_ID = "restservice";
-
-	@Configuration
-	@EnableResourceServer
-	protected static class ResourceServerConfiguration extends
-			ResourceServerConfigurerAdapter {
-
-		@Override
-		public void configure(ResourceServerSecurityConfigurer resources) {
-			// @formatter:off
-			resources
-				.resourceId(RESOURCE_ID);
-			// @formatter:on
-		}
-
-		@Override
-		public void configure(HttpSecurity http) throws Exception {
-			http
-				.authorizeRequests()
-					.antMatchers("/oauth/2").authenticated();
-			
-			http.authorizeRequests()
-			//		.antMatchers("/hb/**").hasRole("REST")	
-					.antMatchers("/favorites").hasRole("USER")					
-					.antMatchers("/favor/**").hasRole("USER")
-			//		.antMatchers("/admin/**").hasRole("ADMIN")
-			        .and()
-			        	.csrf().disable();
-		}
-
-	}
-
-	@Configuration
-	@EnableAuthorizationServer
-	protected static class AuthorizationServerConfiguration extends
-			AuthorizationServerConfigurerAdapter {
-
-		@Autowired
-		private DataSource dataSource;
-		
-		@Autowired
-		@Qualifier("authenticationManagerBean")
-		private AuthenticationManager authenticationManager;
-
-		@Override
-		public void configure(AuthorizationServerEndpointsConfigurer endpoints)
-				throws Exception {
-			// @formatter:off
-			endpoints
-				.tokenStore(tokenStore())
-				.authenticationManager(authenticationManager);
-			// @formatter:on
-		}
-
-		
-		@Override
-		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-			clients.withClientDetails(new JdbcClientDetailsService(dataSource));
-			
-		}
-		
-        @Bean
-        public DefaultTokenServices tokenServices() {
-            DefaultTokenServices tokenServices = new DefaultTokenServices();
-            tokenServices.setSupportRefreshToken(true);
-            tokenServices.setTokenStore(tokenStore());
-            return tokenServices;
-        }
-        
-        @Bean
-        public TokenStore tokenStore() {
-            return new JdbcTokenStore(dataSource);
-        }
-
-    }
-
-}
+//package com.wmanual.web.security;
+//
+///*
+// * Copyright 2014 the original author or authors.
+// *
+// * Licensed under the Apache License, Version 2.0 (the "License");
+// * you may not use this file except in compliance with the License.
+// * You may obtain a copy of the License at
+// *
+// *      http://www.apache.org/licenses/LICENSE-2.0
+// *
+// * Unless required by applicable law or agreed to in writing, software
+// * distributed under the License is distributed on an "AS IS" BASIS,
+// * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// * See the License for the specific language governing permissions and
+// * limitations under the License.
+// */
+//
+//import javax.sql.DataSource;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.authentication.AuthenticationManager;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.http.SessionCreationPolicy;
+//import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+//import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+//import org.springframework.security.oauth2.provider.client.JdbcClientDetailsService;
+//import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+//import org.springframework.security.oauth2.provider.token.TokenStore;
+//import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+//
+//@Configuration
+//public class OAuth2ServerConfiguration {
+//
+//	private static final String RESOURCE_ID = "restservice";
+//
+//	@Configuration
+//	@EnableResourceServer
+//	protected static class ResourceServerConfiguration extends
+//			ResourceServerConfigurerAdapter {
+//
+//		@Override
+//		public void configure(ResourceServerSecurityConfigurer resources) {
+//			// @formatter:off
+//			resources
+//				.resourceId(RESOURCE_ID);
+//			// @formatter:on
+//		}
+//
+//		@Override
+//		public void configure(HttpSecurity http) throws Exception {
+//			http
+//				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+//				.and()
+//					.authorizeRequests()
+//						.antMatchers("/oauth/2").authenticated();
+//		}
+//	}
+//
+//	@Configuration
+//	@EnableAuthorizationServer
+//	protected static class AuthorizationServerConfiguration extends
+//			AuthorizationServerConfigurerAdapter {
+//
+//		@Autowired
+//		private DataSource dataSource;
+//		
+//		@Autowired
+//		@Qualifier("authenticationManagerBean")
+//		private AuthenticationManager authenticationManager;
+//
+//		@Override
+//		public void configure(AuthorizationServerEndpointsConfigurer endpoints)
+//				throws Exception {
+//			// @formatter:off
+//			endpoints
+//				.tokenStore(tokenStore())
+//				.authenticationManager(authenticationManager);
+//			// @formatter:on
+//		}
+//
+//		
+//		@Override
+//		public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+//			clients.withClientDetails(new JdbcClientDetailsService(dataSource));
+//			
+//		}
+//		
+//        @Bean
+//        public DefaultTokenServices tokenServices() {
+//            DefaultTokenServices tokenServices = new DefaultTokenServices();
+//            tokenServices.setSupportRefreshToken(true);
+//            tokenServices.setTokenStore(tokenStore());
+//            return tokenServices;
+//        }
+//        
+//        @Bean
+//        public TokenStore tokenStore() {
+//            return new JdbcTokenStore(dataSource);
+//        }
+//
+//    }
+//
+//}
